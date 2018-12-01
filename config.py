93a32ca4426716f2eaf347c5a753623d0f384424
Pedro Santiago <pedro.santiago16@upr.edu>
2018-11-30 20:01:11 -0400

    Fixed security issue with keys(I regenerated them)

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #Twitter API data
-ACCESS_TOKEN = "852641143204581381-UpEtnk43G0aZA8ky6fyI84tBMK57i4m"
-ACCESS_TOKEN_SECRET = "mlbraOn2BN33zKXyKQoAITcffSr4We9QMJEs1dgI8NhSn"
-CONSUMER_KEY = "gF2EAOKhqyVllar1eA00tn2DQ"
-CONSUMER_SECRET = "16mW0H8FpPYJIoK6bilxWUzV6Gt2OIWqhjlVqlpSg0l7N46xj5"
+ACCESS_TOKEN = "" # access token
+ACCESS_TOKEN_SECRET = "" # access token secret
+CONSUMER_KEY = "" # API Key
+CONSUMER_SECRET = "" #API secret key
 
 HDFS_SERVER = 'http://0.0.0.0:9870'
 OUTPUT_FILE_PATH = "/user/hdfs/tweets/" #TODO
 LIST_PATH = "./"
 
 KEYWORDS = ["Trump", "Flu", "Zika", "Diarrhea", "Ebola", "Headache", "Measles"]
-INTERVAL = 600  # seconds
+INTERVAL = 60  # seconds
