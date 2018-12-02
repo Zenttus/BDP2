60617e2b75f3189f2a6855c22e0e677096dbefcc
Pedro Santiago <pedro.santiago16@upr.edu>
2018-12-02 19:36:16 -0400

    attempt to analyze twitter

--- analyzer.py
+++ analyzer.py
@@ -5,10 +5,10 @@ from pyspark.sql import Row,SQLContext
 import sys
 import requests
 # create spark configuration
-conf = SparkConf()
-conf.setAppName("TwitterStreamApp")
+c = SparkConf()
+c.setAppName("TwitterStreamApp")
 # create spark context with the above configuration
-sc = SparkContext(_conf=conf)
+sc = SparkContext(conf=c)
 sc.setLogLevel("ERROR")
 
 df = spark.read.json("/user/hdfs/tweets/02Dec2018_203919.json")
