818efe1c3f57780dc7852e2c3cacec1a4c56478c
Pedro Santiago <pedro.santiago16@upr.edu>
2018-12-02 19:25:18 -0400

    attempt to analyze twitter

---
+++ analyzer.py
@@ -0,0 +1,15 @@
+# tf = sc.textFile('hdfsPath')
+from pyspark import SparkConf,SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import Row,SQLContext
+import sys
+import requests
+# create spark configuration
+conf = SparkConf()
+conf.setAppName("TwitterStreamApp")
+# create spark context with the above configuration
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+
+df = spark.read.json("/user/hdfs/tweets/02Dec2018_203919.json")
+df.show()
