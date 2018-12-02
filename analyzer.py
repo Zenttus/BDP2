c66aef77f2062da0cc5302af1eb1dbe4ed20e0e9
Pedro Santiago <pedro.santiago16@upr.edu>
2018-12-02 19:56:35 -0400

    attempt to analyze twitter

--- analyzer.py
+++ analyzer.py
@@ -1,15 +1,7 @@
 # tf = sc.textFile('hdfsPath')
-from pyspark import SparkConf,SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.sql import Row,SQLContext
-import sys
-import requests
-# create spark configuration
-c = SparkConf()
-c.setAppName("TwitterStreamApp")
-# create spark context with the above configuration
-sc = SparkContext(conf=c)
-sc.setLogLevel("ERROR")
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Basics').getOrCreate()
 
 df = spark.read.json("/user/hdfs/tweets/02Dec2018_203919.json")
 df.show()
