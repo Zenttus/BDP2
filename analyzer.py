d4e45883aa16ed7b2304031213a96bb08c7a9b3a
Pedro Santiago <pedro.santiago16@upr.edu>
2018-12-02 23:09:57 -0400

    attempt to analyze twitter

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,5 @@
 # tf = sc.textFile('hdfsPath')
 from pyspark import SparkConf, SparkContext
 
-df = sc.read.json("/user/hdfs/tweets/02Dec2018_203919.json")
+df = SparkContext.read.json("/user/hdfs/tweets/02Dec2018_203919.json")
 df.show()
